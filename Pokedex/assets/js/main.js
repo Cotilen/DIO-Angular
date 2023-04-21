a0860ac268354432f735f8a29e51637dd3c61e45
Cleiton <cleitonncruz105@gmail.com>
2023-04-21 16:40:21 -0300

    Pegando todos os pokes

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,39 +1,37 @@
-const criarPokemon = (poke) => {
+const offset = 0
+const limit = 10
+let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    const pokemon = document.createElement('li')
-    pokemon.classList.add('pokemon')
+function pokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
 
-    // const number = document.createElement('span')
-    // number.classList.add('number')
-    // number.textContent = 00
+    <div class="detail">
+        <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+        </ol>
 
-    const name = document.createElement('span')
-    name.classList.add('name')
-    name.textContent = poke.name
-
-    pokemon.append(name)
-
-    return pokemon
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Bulbasaur">
+    </div>
+</li>
+         `
 
 }
 
-const carregarPokemon = async() => {
+const pokemonOl = document.getElementById('pokemons')
 
-    const offset = 0
-    const limit = 10
-    let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            pokemonOl.innerHTML += pokemonToLi(pokemon)
 
-    let response = await fetch(url)
-    let data = await response.json()
-    let pokemon = await data.results
 
-    console.log(url);
-
-    const pokemons = document.getElementById('pokemons')
-
-    const pokes = pokemon.map(criarPokemon)
-
-    pokemons.replaceChildren(...pokes)
-}
-
-carregarPokemon()
+        }
+    })
+    .catch((error) => console.error(error))
