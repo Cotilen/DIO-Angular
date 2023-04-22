641c1a93efac1b2bf558f902fc2372fa873316d1
Cleiton <cleitonncruz105@gmail.com>
2023-04-22 12:26:31 -0300

    Fetchs realixzados

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,20 +1,17 @@
-const offset = 0
-const limit = 10
-let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function pokemonToLi(pokemon) {
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.substring(1).toLowerCase()
     return `
-    <li class="pokemon">
-    <span class="number">#001</span>
-    <span class="name">${pokemon.name}</span>
+    <li class="pokemon ${pokemon.type}">
+    <span class="number">#${pokemon.number}</span>
+    <span class="name">${name}</span>
 
     <div class="detail">
         <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
         </ol>
 
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Bulbasaur">
+        <img src=${pokemon.photo} alt="${name}">
     </div>
 </li>
          `
@@ -23,15 +20,6 @@ function pokemonToLi(pokemon) {
 
 const pokemonOl = document.getElementById('pokemons')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
-        for (let i = 0; i < pokemonList.length; i++) {
-            const pokemon = pokemonList[i];
-            pokemonOl.innerHTML += pokemonToLi(pokemon)
 
-
-        }
-    })
-    .catch((error) => console.error(error))
+pokeApi.getPokemons()
+    .then((pokemons = []) => pokemonOl.innerHTML = pokemons.map(pokemonToLi).join(''))
